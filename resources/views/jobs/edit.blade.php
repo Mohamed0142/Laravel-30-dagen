a7eb8e27fa1615bc27d1e99fe1d52db75a365052
mo121213132131 <hamoudishaaban16@gmail.com>
2024-10-18 14:19:11 +0200

    episode 18 bijna af  bij 14:53 minuten

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -1,11 +1,11 @@
 <x-layout>
     <x-slot:heading>
-        Create Job
+        Edit Job: {{ $job->title }}
     </x-slot:heading>
 
-    <form method="POST" action="/jobs">
+    <form method="POST" action="/jobs/{{ $job->id }}">
         @csrf
-
+        @method('PATCH')
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
                 <h2 class="text-base font-semibold leading-7 text-gray-900">Create a New Job</h2>
@@ -16,7 +16,14 @@
                         <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
                         <div class="mt-2">
                             <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Shift Leader" required>
+                                <input
+                                    type="text"
+                                    name="title"
+                                    id="title"
+                                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="Shift Leader"
+                                    value="{{ $job->title }}"
+                                    required>
                             </div>
                             @error('title')
                             <p class = "text-xs text-red-500 font-semibold">{{ $message }}</p>
@@ -28,7 +35,14 @@
                         <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
                         <div class="mt-2">
                             <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$50,000 Per Year" required>
+                                <input
+                                    type="text"
+                                    name="salary"
+                                    id="salary"
+                                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="$50,000 Per Year"
+                                    value="{{ $job->salary }}"
+                                    required>
                             </div>
                         </div>
                         @error('salary')
@@ -50,8 +64,8 @@
         </div>
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            <a href="/jobs/{{$job->id}}" type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
         </div>
     </form>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+//index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
@@ -15,18 +16,26 @@ Route::get('/jobs', function () {
     ]);
 });
 
+//create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+//show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+//store
 Route::post('/jobs', function () {
-    // validation...
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+        'employer_id' => ['required'],
+
+    ]);
 
     Job::create([
         'title' => request('title'),
@@ -36,6 +45,50 @@ Route::post('/jobs', function () {
 
     return redirect('/jobs');
 });
+//edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+
+//update
+Route::patch('/jobs/{id}', function ($id) {
+  //validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+    //authorize (on hold..)
+
+    //update the job
+
+    $job = Job::findOrFail($id); //returns null
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    // and persist
+    //redirect to the job page
+    return redirect('/jobs/' . $job->id);
+
+
+});
+
+//destroy
+Route::delete('/jobs/{id}', function ($id) {
+    //authorize on hold..!
+
+    //delete the job
+    job::findOrfail($id)->delete();
+
+    //redirect
+    return redirect('/jobs');
+});
+
 
 Route::get('/contact', function () {
     return view('contact');
