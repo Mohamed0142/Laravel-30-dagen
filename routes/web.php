85a812dded6bbcda306e1f1872e295ea9971b5ea
mo121213132131 <hamoudishaaban16@gmail.com>
2024-10-25 13:55:38 +0200

    21 af

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
@@ -8,4 +10,9 @@ Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
 
+// auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
 
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/register', [SessionController::class, 'store']);
